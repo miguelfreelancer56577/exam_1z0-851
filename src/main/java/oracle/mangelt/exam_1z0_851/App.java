b472580c5cfb84bd0178ea49fda4687f10a523ba
miguelfreelancer56577 <miguelfreelancer56577@gmail.com>
2017-01-19 15:30:11 -0600

    example of thread

--- src/main/java/oracle/mangelt/exam_1z0_851/App.java
+++ src/main/java/oracle/mangelt/exam_1z0_851/App.java
@@ -4,10 +4,17 @@ package oracle.mangelt.exam_1z0_851;
  * Hello world!
  *
  */
-public class App 
+public class App implements Runnable 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	Thread t = new Thread(new App());
+    	t.start();
+        System.out.println( "End of method" );
+    }
+    
+    public void run(){
+    	System.out.println("run...");
+    	throw new RuntimeException("a problem occur");
     }
 }
