497126eab3e0375bbff39107bc24de6b3e3a2ebb
miguelfreelancer56577 <miguelfreelancer56577@gmail.com>
2017-01-19 15:39:15 -0600

    IllegalMonitorStateException.

--- src/main/java/oracle/mangelt/exam_1z0_851/App.java
+++ src/main/java/oracle/mangelt/exam_1z0_851/App.java
@@ -8,13 +8,27 @@ public class App implements Runnable
 {
     public static void main( String[] args )
     {
-    	Thread t = new Thread(new App());
-    	t.start();
-        System.out.println( "End of method" );
+//    	Thread t = new Thread(new App());
+//    	t.start();
+//        System.out.println( "End of method" );
+    	waitForSignal();
     }
     
     public void run(){
     	System.out.println("run...");
     	throw new RuntimeException("a problem occur");
     }
+    
+    public static void waitForSignal() {
+		Object obj = new Object();
+		synchronized (Thread.currentThread()) {
+			try {
+				obj.wait();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			obj.notify();
+		}
+	}
 }
