2ebdd06c458d81e64890caa58be092337294ee64
miguelfreelancer56577 <miguelfreelancer56577@gmail.com>
2017-01-20 11:54:34 -0600

    question 16

---
+++ src/main/java/oracle/mangelt/exam_1z0_851/Blip.java
@@ -0,0 +1,7 @@
+package oracle.mangelt.exam_1z0_851;
+
+public class Blip {
+	
+	protected int blipvert(int x) { return 0; }
+	
+}
---
+++ src/main/java/oracle/mangelt/exam_1z0_851/Vert.java
@@ -0,0 +1,21 @@
+package oracle.mangelt.exam_1z0_851;
+
+public class Vert extends Blip {
+	
+//	public int blipvert(int x) { return 0; }
+	
+//	Cannot reduce the visibility of the inherited method from Blip
+//	private int blipvert(int x) { return 0; }
+	
+//	private int blipvert(long x) { return 0; }
+	
+//	The return type is incompatible with Blip.blipvert(int)
+//	protected long blipvert(int x) { return 0; }
+	
+//	protected int blipvert(long x) { return 0; }
+	
+//	protected long blipvert(long x) { return 0; }
+	
+	protected long blipvert(int x, int y) { return 0; }
+	
+}
