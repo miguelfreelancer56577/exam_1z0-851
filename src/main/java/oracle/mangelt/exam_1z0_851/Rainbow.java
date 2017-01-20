29618ee6d8d68b67edde73505d189670a6585bcb
miguelfreelancer56577 <miguelfreelancer56577@gmail.com>
2017-01-20 11:49:32 -0600

    question 14

--- src/main/java/oracle/mangelt/exam_1z0_851/Rainbow.java
+++ src/main/java/oracle/mangelt/exam_1z0_851/Rainbow.java
@@ -1,16 +1,20 @@
 package oracle.mangelt.exam_1z0_851;
 
+interface TestA{
+	
+}
+
 public class Rainbow {
-	enum MyColor {
-		RED(0xff0000), GREEN(0x00ff00), BLUE(0x0000ff);
-		private final int rgb;
-		MyColor(int rgb) { this.rgb = rgb; }
-		public int getRGB() { return rgb; }
-	};
 		
 	public static void main(String[] args) {
-	//insert code here
-//		MyColor skyColor = BLUE;
-		MyColor treeColor = MyColor.GREEN;
+		
+		System.out.println(new TestA() {
+			@Override
+			public String toString() {
+				// TODO Auto-generated method stub
+				return "TestA";
+			} 
+		});
+		
 	}
 }
