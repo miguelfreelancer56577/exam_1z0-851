bc0376730063b7d08e3fbe1d6062b4f0d195e83d
miguelfreelancer56577 <miguelfreelancer56577@gmail.com>
2017-01-20 12:02:13 -0600

    question 21

--- src/main/java/oracle/mangelt/exam_1z0_851/Barn.java
+++ src/main/java/oracle/mangelt/exam_1z0_851/Barn.java
@@ -1,14 +1,20 @@
 package oracle.mangelt.exam_1z0_851;
 
-public class Barn {
+
+class Building { }
+
+public class Barn extends Building {
 	
 	public static void main(String[] args) {
-		new Barn().go("hi", 1);
-		new Barn().go("hi", "world", 2);
+		
+		Building build1 = new Building();
+		Barn barn1 = new Barn();
+		Barn barn2 = (Barn) build1;
+		Object obj1 = (Object) build1;
+//		Cannot cast from Building to String
+//		String str1 = (String) build1;
+		Building build2 = (Building) barn1;
+		
 	}
 	
-	public void go(String... y, int x) {
-	System.out.print(y[y.length - 1] + " ");
-	}
-
 }
