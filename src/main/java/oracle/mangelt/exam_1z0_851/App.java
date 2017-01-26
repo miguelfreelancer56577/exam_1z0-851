c0f529279edbac70e99bbcbf1c6a4f29fb6678f8
miguelfreelancer56577 <miguelfreelancer56577@gmail.com>
2017-01-26 17:19:16 -0600

    question 40

--- src/main/java/oracle/mangelt/exam_1z0_851/App.java
+++ src/main/java/oracle/mangelt/exam_1z0_851/App.java
@@ -13,15 +13,22 @@ public class App
     public static void main( String[] args )
     {
     	
-    	double d = 12.345;
-    	System.out.printf("|%.3f| \n", d);
-    	System.out.printf("|%3.10f| \n", d);
-    	System.out.printf("|%2.7f| \n", d);
-    	System.out.printf("|%7.1f| \n", d);
-    	System.out.printf("|%6f| \n", d);
-    	System.out.printf("|%5f| \n", d);
-    	System.out.printf("|%4f| \n", d);
-    	System.out.printf("|%3f| \n", d);
+    	go();
     	
     }
+    
+    public static void go() {
+    	String o = "";
+    	z:
+    	for(int x = 0; x < 3; x++) {
+    		z2:
+	    	for(int y = 0; y < 2; y++) {
+		    	if(x==1) break z2;
+		    	if(x==2 && y==1) break z;
+		    	o = o + x + y;
+	    	}
+    	}
+    	System.out.println(o);
+	}
+    
 }
