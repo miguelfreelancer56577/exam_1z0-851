18012a08520fe5a3367899c4be32224e85ab270a
miguelfreelancer56577 <miguelfreelancer56577@gmail.com>
2017-02-20 15:21:52 -0600

    Exam B, question 12, I understand.

--- src/main/java/oracle/mangelt/exam_1z0_851/App.java
+++ src/main/java/oracle/mangelt/exam_1z0_851/App.java
@@ -3,37 +3,29 @@ package oracle.mangelt.exam_1z0_851;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.util.Date;
+import java.util.PriorityQueue;
 
 /**
  * Hello world!
  *
  */
 
-class ClassA {
-	public int numberOfInstances;
-	protected ClassA(int numberOfInstances) {
-		this.numberOfInstances = numberOfInstances;
-	}
-}
 
-public class App extends ClassA
+public class App 
 {
-    protected App(int numberOfInstances) {
-		super(numberOfInstances);
-	}
 
 	public static void main( String[] args )
     {
-    	App a = new App(3);
-    	System.out.print(a.numberOfInstances);
+		PriorityQueue<String> pq = new PriorityQueue<String>();
+		pq.add("banana");
+		pq.add("pear");
+		pq.add("apple");
+		System.out.println(pq);
+		System.out.println(pq.poll() + " " + pq.peek());
+		System.out.println(pq);
     }
     
 }
 
-class ClassB{
-	public static void main(String[] args) {
-		App a = new App(4);
-	}
-}
 
 
