c1244e6927a46c7f6ce38a0d4e534d171268fa62
miguelfreelancer56577 <miguelfreelancer56577@gmail.com>
2017-03-02 08:27:24 -0600

    Exam B, question 16

--- src/main/java/oracle/mangelt/exam_1z0_851/App.java
+++ src/main/java/oracle/mangelt/exam_1z0_851/App.java
@@ -2,6 +2,7 @@ package oracle.mangelt.exam_1z0_851;
 
 import java.text.DateFormat;
 import java.text.ParseException;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.PriorityQueue;
 import java.util.TreeSet;
@@ -15,31 +16,22 @@ import java.util.TreeSet;
 public class App 
 {
 	
-enum Regex{
-		
-		NONUMERIC("\\D");
-		
-		private String ex;
-		
-		Regex(String ex){
-			this.ex = ex;
-		}
-		
-		public String getEx() {
-			return ex;
-		}
-	}
-	
 	public static void main( String[] args )
     {
 		
-		String regex = "\\D";
+		String[] colors = {"blue", "red", "green", "yellow", "orange"};
+		for (int i = 0; i < colors.length; i++) {
+			System.out.println("no sort: " + colors[i]);
+		}
+		Arrays.sort(colors);
+		for (int i = 0; i < colors.length; i++) {
+			System.out.println("sort: " + colors[i]);
+		}
+		int s2 = Arrays.binarySearch(colors, "orange");
+		int s3 = Arrays.binarySearch(colors, "violet");
+		int s4 = Arrays.binarySearch(colors, "brown");
 		
-		String replacement = "TRF8709701----------+++ñññññññññññññññ313";
-		
-		System.out.println(replacement.replaceAll(regex, ""));
-		
-		System.out.println(Regex.NONUMERIC.getEx());
+		System.out.println(s2 + " " + s3 + " " + s4);
 		
     }
     
