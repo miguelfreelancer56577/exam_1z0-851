5e8e584df66986df5880f53572df5afef1ed372e
miguelfreelancer56577 <miguelfreelancer56577@gmail.com>
2017-03-08 17:08:49 -0600

    Exam B, question 17

--- src/main/java/oracle/mangelt/exam_1z0_851/App.java
+++ src/main/java/oracle/mangelt/exam_1z0_851/App.java
@@ -4,7 +4,9 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.PriorityQueue;
+import java.util.Set;
 import java.util.TreeSet;
 
 /**
@@ -19,19 +21,13 @@ public class App
 	public static void main( String[] args )
     {
 		
-		String[] colors = {"blue", "red", "green", "yellow", "orange"};
-		for (int i = 0; i < colors.length; i++) {
-			System.out.println("no sort: " + colors[i]);
-		}
-		Arrays.sort(colors);
-		for (int i = 0; i < colors.length; i++) {
-			System.out.println("sort: " + colors[i]);
-		}
-		int s2 = Arrays.binarySearch(colors, "orange");
-		int s3 = Arrays.binarySearch(colors, "violet");
-		int s4 = Arrays.binarySearch(colors, "brown");
-		
-		System.out.println(s2 + " " + s3 + " " + s4);
+		HashMap props = new HashMap();
+		props.put("key45", "some value");
+		props.put("key12", "some other value");
+		props.put("key39", "yet another value");
+		Set s = props.keySet();
+//		s = new TreeSet(s);
+		System.out.println(s);
 		
     }
     
