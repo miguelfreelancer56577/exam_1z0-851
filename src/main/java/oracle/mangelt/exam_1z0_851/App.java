c39169d95dd7522b31ab0364e272dad69cc44b63
miguelfreelancer56577 <miguelfreelancer56577@gmail.com>
2017-03-08 17:24:52 -0600

    Exam B, question 17

--- src/main/java/oracle/mangelt/exam_1z0_851/App.java
+++ src/main/java/oracle/mangelt/exam_1z0_851/App.java
@@ -18,7 +18,8 @@ import java.util.TreeSet;
 public class App 
 {
 	
-	public static void main( String[] args )
+	
+	public static void synchronizedmain( String[] args )
     {
 		
 		HashMap props = new HashMap();
@@ -30,6 +31,10 @@ public class App
 		System.out.println(s);
 		
     }
+	
+	public synchronized static void thred(){
+		
+	}
     
 }
 
