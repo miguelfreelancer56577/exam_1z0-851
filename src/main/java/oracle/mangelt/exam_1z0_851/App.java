32382b594bf7460ec6f8c770c9fdb703de64b2c5
miguelfreelancer56577 <miguelfreelancer56577@gmail.com>
2017-03-08 17:40:23 -0600

    Exam B, question 23

--- src/main/java/oracle/mangelt/exam_1z0_851/App.java
+++ src/main/java/oracle/mangelt/exam_1z0_851/App.java
@@ -21,39 +21,27 @@ public class App
 	public static void main( String[] args )
     {
 		
-		new TestFive().go();
+		Runnable r = new Runnable() {
+			
+			public void run() {
+				System.out.println("cat");
+			}
+			
+		};
+		
+		Thread t = new Thread(r){
+			
+			public void run(){
+				System.out.println("dog");
+			}
+			
+		};
+		
+		t.start();
+		
 		
     }
 	
-}
-
-class TestFive {
-	
-   private int x;
-  
-   public void foo() {
-	   synchronized (this) {
-		   int current = x;
-		   x = current + 1;
-		   System.out.print(x + ", ");
-	   }
-   }
-   
-//   public synchronized void foo() {
-//	   int current = x;
-//	   x = current + 1;
-//	   System.out.println(x + ", ");
-//   }
-  
-   public void go() {
-	   for(int i = 0; i < 100; i++) {
-		   new Thread() {
-			   public void run() {
-				   foo();
-			   }
-		   }.start();
-	   }
-   }
 }
 
 
