f1389747efa7de4881ceabe90656a9df54accf8d
miguelfreelancer56577 <miguelfreelancer56577@gmail.com>
2017-03-09 17:29:25 -0600

    Exam b, question 47

--- src/main/java/oracle/mangelt/exam_1z0_851/App.java
+++ src/main/java/oracle/mangelt/exam_1z0_851/App.java
@@ -9,29 +9,20 @@ import java.util.PriorityQueue;
 import java.util.Set;
 import java.util.TreeSet;
 
-/**
- * Hello world!
- *
- */
-
 
 public class App 
 {
 	
 	public static void main( String[] args )
     {	
-//		Syntax error on token "Invalid Character"
-//		String #name = "Jane Doe";
-		int $age = 24;
-		Double _height = 123.5;
-//		Syntax error on token "Invalid Character"
-//		double ~temp = 37.5;
 		
+		boolean assertsOn = false;
+		assert (true) : assertsOn = true;
+		if(assertsOn) {
+			System.out.println("assert is on");
+		}
 		
     }
 	
 }
-
-
-
 
