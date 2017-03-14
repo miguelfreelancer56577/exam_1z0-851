cd3a02dd6b5f77c4ddd4977795f95a68e80ecaa6
miguelfreelancer56577 <miguelfreelancer56577@gmail.com>
2017-03-14 16:08:32 -0600

    Exam B, question 56

--- src/main/java/oracle/mangelt/exam_1z0_851/App.java
+++ src/main/java/oracle/mangelt/exam_1z0_851/App.java
@@ -2,9 +2,11 @@ package oracle.mangelt.exam_1z0_851;
 
 import java.text.DateFormat;
 import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.PriorityQueue;
 import java.util.Set;
 import java.util.TreeSet;
@@ -16,25 +18,15 @@ public class App
 	public static void main( String[] args )
     {	
 		
-		try {
-			new A().sayHello("Aiko");
-		} catch (TestException e) {
-			e.printStackTrace();
-		}
+		
+		StringBuffer s = new StringBuffer("123456789");//123456789,456789,424789,4247
+		
+		s.delete(0,3);
+		s.delete(1,3);
+		s.delete(2,5);
+		s.insert(1, "24");
 		
     }
 	
-	
-	
 }
-
-
-class TestException extends Exception { }
-class A {
-	public String sayHello(String name) throws TestException {
-		if(name == null) throw new TestException();
-		return "Hello " + name;
-	}
-}
-
 
